9799d29eaa82ce3f5db718e692c86c1d8b0487c5
CodeDragonite <69617110+CodeDragonite@users.noreply.github.com>
2025-02-25 15:26:10 +0000

    WIP6.3

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/GlobalExHandler.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/GlobalExHandler.java
@@ -11,8 +11,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice
-public class GlobalExHandler extends ResponseEntityExceptionHandler{
+public class GlobalExHandler extends ResponseEntityExceptionHandler {
 
   /**
    * Leverage Exception Handler framework for resource not found Exception.
@@ -24,7 +28,7 @@ public class GlobalExHandler extends ResponseEntityExceptionHandler{
   @ExceptionHandler(ResourceNotFoundException.class)
   public final ResponseEntity<Object> handleResourceNotFoundException(
       ResourceNotFoundException ex, WebRequest request) {
-
+    logException(ex);
     ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
     return createResponseEntity(pd, null, HttpStatus.NOT_FOUND, request);
   }
@@ -39,6 +43,7 @@ public class GlobalExHandler extends ResponseEntityExceptionHandler{
   @ExceptionHandler(NoSuchElementException.class)
   public final ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException ex, WebRequest request) {
 
+    logException(ex);
     ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
     return createResponseEntity(pd, null, HttpStatus.NOT_FOUND, request);
   }
@@ -52,8 +57,13 @@ public class GlobalExHandler extends ResponseEntityExceptionHandler{
    */
   @ExceptionHandler(Exception.class)
   public final ResponseEntity<Object> handleNoSuchElementException(Exception ex, WebRequest request) {
+    logException(ex);
     ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     return createResponseEntity(pd, null, HttpStatus.INTERNAL_SERVER_ERROR, request);
+  }
+
+  private void logException(Exception ex) {
+    log.error("Caught Exception", ex);
   }
 
 }
